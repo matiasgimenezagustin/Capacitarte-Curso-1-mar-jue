7568d31ed78f760b474df01fc7e3a418f4b31bd3
matu.dev <matu.a.gimenez2002@gmail.com>
2023-09-19 15:56:03 -0300

    clase 4

--- CURSADA JS INICIAL Mar y Jue/clase_4/productos.js
+++ CURSADA JS INICIAL Mar y Jue/clase_4/productos.js
@@ -1,4 +1,7 @@
 
+function calcularIva (precio){
+    return precio * 1.21
+}
 
 const productos = [
     {
@@ -22,20 +25,37 @@ const productosContenedor = document.getElementById('productos')
 
 function renderizarProductos (productos){
     let productosHTML = ''
-    for(const producto of productos){
-        productosHTML += `
-            <div>
-                <h2>${producto.nombre}</h2>
-                <h3>Precio: $${producto.precio}</h3>
-                <h3>Stock: ${producto.stock}</h3>
-                <button>Comprar</button>
-                <hr>
-            </div>
-        `
+    if(productos.length == 0){
+        productosHTML = 'No hay resultados disponibles para tu busqueda :('
+    }else{
+        for(const producto of productos){
+            productosHTML += `
+                <div>
+                    <h2>${producto.nombre}</h2>
+                    <h3>Precio: $${producto.precio}</h3>
+                    <h3>Precio con iva: $${calcularIva(producto.precio)}</h3>
+                    <h3>Stock: ${producto.stock}</h3>
+                    <button>Comprar</button>
+                    <hr>
+                </div>
+            `
+        }
+        
     }
     productosContenedor.innerHTML = productosHTML
 }
 
-renderizarProductos(productos)
+function filtrarPorMenorA(precioMaximo, productos){
+    const result = []
+    for(const producto of productos){
+        if(producto.precio <=precioMaximo){
+            result.push(producto)
+        }
+    }
+    return result
+    
+}
+let precioMaximo = prompt('ingresa un valor maximo que estas dispuesto a gastar')
+renderizarProductos(filtrarPorMenorA(precioMaximo, productos))
 
 /* Utilizando el mismo array de carrito van a generar un div con id carrito en html, lo van a llamar y van a renderizar dentro de el todos los items */
