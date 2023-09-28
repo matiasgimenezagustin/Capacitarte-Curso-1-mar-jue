6010e9bed988fc516fbfa3e8d6b71afcc5e60284
matu.dev <matu.a.gimenez2002@gmail.com>
2023-09-28 15:22:29 -0300

    clase 7

--- CURSADA JS INICIAL Mar y Jue/clase_7/script.js
+++ CURSADA JS INICIAL Mar y Jue/clase_7/script.js
@@ -5,7 +5,23 @@ const formlarioHTML = document.getElementById('formularioRegistro')
 }) */
 formlarioHTML.addEventListener('submit', (evento)=>{
     evento.preventDefault()
-    console.log(formlarioHTML.contrasenia.value)
-    console.log(formlarioHTML.username.value)
-    console.log('se envio mi formulario')
+    let password = formlarioHTML.contrasenia.value
+    let username = formlarioHTML.username.value
+    localStorage.setItem('user', JSON.stringify({password: password, username: username}))
+    if(username == 'pepe' && password == 'admin1234'){
+        window.location.href = '/home.html'
+    }
+    else{
+        const errorHTML = document.getElementById('errorText')
+        errorHTML.innerHTML = 'Usuario o Contraseña no valido'
+    }
 })
+
+/* Teniendo en cuenta que tendras un formulario con
+
+nombre
+edad
+contrasenia
+
+Si el nombre es ´juan´ la edad es '28' y la contrasenia es 'admin1234' vas a decir por alerta 'hola jefe', sino diras por alerta 'fuera de aqui'
+*/
